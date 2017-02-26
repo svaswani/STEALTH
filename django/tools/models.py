a0a6e342a8e36347efe055f0888bfd08d0c59699
Shannon McIntosh <smcintosh881@gmail.com>
2017-02-25 20:27:45 -0500

    account creation

--- django/tools/models.py
+++ django/tools/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+import datetime
+from django.core.exceptions import ValidationError
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
@@ -7,3 +11,10 @@ class Post(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+class BaseUser( User ):
+
+    address = models.CharField( max_length = 300 )
+
+    def __str__( self ):
+        return self.username
